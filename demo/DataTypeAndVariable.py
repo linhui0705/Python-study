8928755e6fd3caf416a0c9f93446e9567fba8eb5
linhui0705 <linhui0705@gmail.com>
2018-01-11 18:38:43 +0800

    201801111838

---
+++ demo/DataTypeAndVariable.py
@@ -0,0 +1,87 @@
+'''
+字符串
+'''
+print('【字符串】')
+#I'm OK.
+print('I\'m OK.')
+print('--------------------')
+'''
+I'm learning Python.
+It's interesting!
+'''
+print('I\'m learning Python\nIt\'s interesting!')
+print('--------------------')
+'''
+\	\
+\\\t\\
+'''
+print('\\\t\\')
+print(r'\\\t\\')
+print('--------------------')
+'''
+line1
+line2
+line3
+'''
+print('''line1
+line2
+line3''')
+print('----------------------------------------')
+
+'''
+布尔值
+True
+False
+True
+False
+[and]
+True
+False
+False
+True
+[or]
+True
+True
+False
+True
+[not]
+False
+True
+True
+'''
+print('【布尔值】')
+print(True)
+print(False)
+print(3>2)
+print(3>5)
+#布尔值可以用and、or和not运算
+print("[and]")
+print(True and True)
+print(True and False)
+print(False and False)
+print(5>3 and 3>1)
+print("[or]")
+print(True or True)
+print(True or False)
+print(False or False)
+print(5>3 or 3>1)
+print("[not]")
+print(not True)
+print(not False)
+print(not 1>2)
+print('----------------------------------------')
+
+#空值是Python里的特殊值，用None表示。None不能理解为0，因为0是有意义的，而None是一个特殊的值
+
+'''
+变量
+
+'''
+print('【变量】')
+a=1
+t_007='T007'
+Answer=True
+print(a)
+print(t_007)
+print(Answer)
+print('----------------------------------------')
